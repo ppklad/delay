df09fe9c21f0d921a4167c63dff583d5a226e3fe
ppklad <ppklad@ya.ru>
2021-06-08 08:47:27 +0300

    try5

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const cool = require('cool-ascii-faces');
-// const sleep = require('sleep');
-const sleep = require('./wait');
+const sleep = require('sleep');
+const wait = require('./wait');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+
+const wait2s = () => { sleep.sleep(2); return 'ok' }
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -11,5 +13,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  .get('/wait', (req, res) => res.send(sleep.getSleep()))
+  // .get('/wait', (req, res) => res.send(wait.getSleep()))
+  .get('/wait', (req, res) => res.send(wait2s()))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
